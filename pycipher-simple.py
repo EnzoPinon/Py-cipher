f905fb20e1dc9dcfc35e339626d8961415b80377
EnzoPinon <mrlorenzopinon@gmail.com>
2023-04-21 00:52:25 +0800

    Request input

--- pycipher-simple.py
+++ pycipher-simple.py
@@ -6,3 +6,8 @@ user_stop = False
 
 # simple greeting!
 print("Hi there! I am PyCipher-Simple! I only encrypt and decrypt vowels.")
+
+while user_stop == False:
+
+    # ask user to input an encrypted statement.
+    encrypted = input("Input a statement that is already encrypted, or say 'stop' to stop: ")
