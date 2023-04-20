7cb78f9d81b41d9a8f8abbe49be9fab5767b165e
EnzoPinon <mrlorenzopinon@gmail.com>
2023-04-21 00:55:16 +0800

    add stop function in simple cipher

--- pycipher-simple.py
+++ pycipher-simple.py
@@ -11,3 +11,15 @@ while user_stop == False:
 
     # ask user to input an encrypted statement.
     encrypted = input("Input a statement that is already encrypted, or say 'stop' to stop: ")
+    
+     # check if the user wanted to stop
+
+    if encrypted.lower() == "stop":
+
+        #set the initial variable to True to break the loop
+
+        user_stop = True
+
+        # say goodbye!
+    
+        print("Goodbye and hope to see you again!")
