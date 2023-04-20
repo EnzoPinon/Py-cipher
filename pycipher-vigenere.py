527159a83bf17f9b32fc6d143dfc5631c74ff833
EnzoPinon <mrlorenzopinon@gmail.com>
2023-04-21 00:34:10 +0800

    adding the option to close the app

--- pycipher-vigenere.py
+++ pycipher-vigenere.py
@@ -4,3 +4,15 @@ user_stop = False
 
 # greet the user
 print("Welcome to the Vigenere Cipher maker!")
+
+while user_stop == False:
+
+    request_type = str(input("Indicate if this is to encode or decode, or 'stop' to stop the process: "))
+    #check if it is to stop
+    print(request_type)
+    if request_type.lower() == 'stop':
+        user_stop = True
+        print("Stopping! See you soon!")
+        break
+
+    
