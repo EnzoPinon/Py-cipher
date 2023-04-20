ef457d752fe8f15980857c8f16209ff04f82c2bf
EnzoPinon <mrlorenzopinon@gmail.com>
2023-04-21 01:24:20 +0800

    Added encrypter function and fixed typo errors

--- pycipher-simple.py
+++ pycipher-simple.py
@@ -23,10 +23,26 @@ while user_stop == False:
         # say goodbye!
     
         print("Goodbye and hope to see you again!")
+    
+    #encoder
+    if encrypted.lower() == "encrypt":
+        #ask for word to encrypt
+        to_encrypt = input("You have chosen 'encrypt'! Please type the message to encode: ")
+
+        #replace every vowel with the equivalent cipher symbol.
+        first_vowel = to_encrypt.replace('a', '*')
+        second_vowel = first_vowel.replace('e', '&')
+        third_vowel = second_vowel.replace('i', '#')
+        fourth_vowel = third_vowel.replace('o', '+')
+        encrypt_final = fourth_vowel.replace('u', '!')
+
+        #print the result!
+        print("Original text: ", to_encrypt)
+        print("Encrypted version: ", encrypt_final)
 
     if encrypted.lower() == "decrypt":
         # ask for word to decrypt
-        todecypt = input("You have chosen 'decrypt'! Please type the message to decode.")
+        todecypt = input("You have chosen 'decrypt'! Please type the message to decode: ")
 
         # attempt to replace every encrypted symbol with an appropriate equivalent.
         first_vowel = todecypt.replace('*', 'a')
