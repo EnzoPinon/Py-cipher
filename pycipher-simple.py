36d27eb51f0836e36a25841f7eee9ebf8d0555c7
EnzoPinon <mrlorenzopinon@gmail.com>
2023-04-20 14:59:39 +0800

    preface for the simple decoder

--- pycipher-simple.py
+++ pycipher-simple.py
@@ -1,2 +1,8 @@
 # this activity will allow encryption and decryption of vowels only.
 
+# define when to stop
+
+user_stop = False
+
+# simple greeting!
+print("Hi there! I am PyCipher-Simple! I only encrypt and decrypt vowels.")
